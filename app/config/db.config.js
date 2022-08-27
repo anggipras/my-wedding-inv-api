e1acc04b11dbab702cd15d48e471b7c4a5c6c62f
anggi.prastianto <anggiprastianto30@gmail.com>
2022-08-27 22:07:34 +0700

    setup api server

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "YOUR_DATABASE_URL",
-  USER: "YOUR_USER",
-  PASSWORD: "YOUR_PASSWORD",
-  DB: "YOUR_DATABASE_NAME",
+  HOST: "satao.db.elephantsql.com",
+  USER: "vzvuxftd",
+  PASSWORD: "5c0yeoZ9AZ6u9JF-BHNbAbCxhbXvvOiE",
+  DB: "vzvuxftd",
   dialect: "postgres",
   pool: {
     max: 5,
